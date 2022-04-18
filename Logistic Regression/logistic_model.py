be76dc6f56677d9aee893f7c4bd06eb4b18461e3
raufodilov <applyjobrauf@gmail.com>
2022-04-18 20:25:03 +0200

    Updated

--- Logistic Regression/logistic_model.py
+++ Logistic Regression/logistic_model.py
@@ -10,4 +10,20 @@ sns.set(style="white")
 sns.set(style="whitegrid", color_codes=True)
 
 # load data
-data = pd.read_csv("G:/rauf/STEPBYSTEP/Tutorial/Repeat Knowledge/ML/Logistic Regression/banking.csv", header=0)
+data = pd.read_csv("G:/rauf/STEPBYSTEP/Tutorial/Repeat Knowledge/ML/Logistic Regression/logistic_regression_data.csv")
+print(data)
+x = data.iloc[:,:-1].values
+y = data.iloc[:, -1].values
+print(x, y)
+# create model
+model = LogisticRegression()
+
+#model training
+model.fit(x, y)
+
+# model predicting
+result_posetive = model.predict([[85]])
+print(result_posetive) # model correctly prediced because i entered age 85 which likely buy insurance 
+
+result_negative = model.predict([[18]]) # model has to predict likely negative because our client is young
+print(result_negative)
